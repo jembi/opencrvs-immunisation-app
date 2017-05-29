e1a22592fa385e2687df586aaeb7feb11a39bac6
Andrew <napierandr@gmail.com>
2017-05-29 15:00:23 +0200

    RCBS-22 remove unecessary injection

--- app/scripts/directives/index.js
+++ app/scripts/directives/index.js
@@ -2,5 +2,5 @@
 
 var app = require('angular').module('rcbsApp')
 
-app.directive('dashboardHeader', [ 'Api', 'loadResource', require('./dashboard-header') ])
+app.directive('dashboardHeader', require('./dashboard-header'))
 app.directive('searchById', require('./search-by-id'))
