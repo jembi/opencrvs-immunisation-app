19d178d67a66078db80f8f5796b1cc4c8f855fac
Andrew <napierandr@gmail.com>
2017-05-29 11:57:00 +0200

    RCBS-22: Scaffold directive for search patient by id

--- app/scripts/directives/index.js
+++ app/scripts/directives/index.js
@@ -3,3 +3,4 @@
 var app = require('angular').module('rcbsApp')
 
 app.directive('dashboardHeader', require('./dashboard-header'))
+app.directive('searchById', require('./search-by-id'))
---
+++ app/scripts/directives/search-by-id/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = function () {
+  return {
+    restrict: 'EA',
+    templateUrl: 'app/scripts/directives/search-by-id/view.html',
+    scope: {},
+    link: function (scope) {}
+  }
+}
