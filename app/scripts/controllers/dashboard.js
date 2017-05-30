299f3287d9b150a61b34c28a857a0a70710f5c29
Martin Brocker <martin@jembi.org>
2017-05-30 08:36:31 +0200

    RCBS-16: removed testing API call

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,13 +1,4 @@
 'use strict'
 
 module.exports = function ($scope, Api) {
-  // Authorised request
-  var success = function (result) {
-    console.log(result)
-  }
-
-  var error = function (err) {
-    console.log(err)
-  }
-  Api.Patients.get(success, error)
 }
