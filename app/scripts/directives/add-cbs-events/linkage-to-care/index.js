3c1667257932bfa9ab3da8966969a0852c97382b
Andrew <napierandr@gmail.com>
2017-06-02 15:19:31 +0200

    RCBS-24: Correct the formbuilder property

--- app/scripts/directives/add-cbs-events/linkage-to-care/index.js
+++ app/scripts/directives/add-cbs-events/linkage-to-care/index.js
@@ -30,7 +30,7 @@ module.exports = function (Api, loadResource, $q) {
         displayType: null,
         globals: {},
         sections: [],
-        login: {},
+        linkageToCareForm: {},
         buttons: {
           submit: 'add event'
         },
