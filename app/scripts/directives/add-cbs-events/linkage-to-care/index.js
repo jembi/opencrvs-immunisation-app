b4f444106a153263fd2001e989180285b723069e
Martin Brocker <martin@jembi.org>
2017-06-05 14:58:41 +0200

    RCBS-28: Add subject reference to event, resetForm function

--- app/scripts/directives/add-cbs-events/linkage-to-care/index.js
+++ app/scripts/directives/add-cbs-events/linkage-to-care/index.js
@@ -4,7 +4,9 @@ module.exports = function (Api, loadResource, $q, state, FHIR) {
   return {
     restrict: 'EA',
     templateUrl: 'app/scripts/directives/add-cbs-events/linkage-to-care/view.html',
-    scope: {},
+    scope: {
+      subjectReference: '@'
+    },
     link: function (scope) {
       var submit = function (form) {
         var defer = $q.defer()
@@ -22,16 +24,37 @@ module.exports = function (Api, loadResource, $q, state, FHIR) {
           var fhirObject = FHIR.mapFHIRObject(fhirDoc, scope.state.FormBuilderAddCbsEventLinkageToCare, formFieldsValues)
 
           // add the Subject Refernce - Patient/Reference
-          fhirObject.subject.reference = 'patient/link'
+          fhirObject.subject.reference = scope.subjectReference
+
+          // TODO: Add document to state bundle for submission
+          state.addEventToBundle(fhirObject)
+          
+          scope.resetForm(scope.state.FormBuilderAddCbsEventLinkageToCare, form)
 
           defer.resolve({ isValid: true, msg: 'Event mapped to FHIR document!' })
-
-          // TODO: API call to submit document
-          // OR - Save to holding object to be sent in bundle
         })
 
         return defer.promise
       }
+
+      scope.resetForm = function(formSchema, form) {
+        for (var fbs = 0; fbs < formSchema.sections.length; fbs++) {
+          var section = formSchema.sections[fbs]
+
+          for (var fbr = 0; fbr < section.rows.length; fbr++) {
+            var row = section.rows[fbr]
+
+            for (var fbf = 0; fbf < row.fields.length; fbf++) {
+              var field = row.fields[fbf]
+              field.value = null // remove values from ngModel defined in FormBuilder schema
+            }
+          }
+        }
+
+        // remove validation errors
+        form.$setPristine();
+        form.$setUntouched();
+      };
 
       scope.state = {}
       scope.state.FormBuilderAddCbsEventLinkageToCare = {
