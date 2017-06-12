0e24014d6315e177cf41280c562c14999f0ff336
Andrew <napierandr@gmail.com>
2017-06-12 16:48:11 +0200

    RCBS-58: Constuct simple cd4-count object from encounter

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -1,9 +1,27 @@
 'use strict'
 
 module.exports = function () {
+  const CD4_COUNT = 'cd4-count'
   return {
     test: () => {
       console.log('Event service test')
+    },
+    
+    constructSimpleCD4CountObject: (encounter) => {
+      return {
+        eventType: CD4_COUNT,
+        encounter: {
+          date: encounter.period.start,
+          type: encounter.type[0].coding[0].display,
+          location: encounter.location[0].location.display,
+        },
+        observations: {
+          cd4CountResult: encounter._observations[0].
+        },
+        practitioner: {
+          practitioner: 'TODO'
+        }
+      }
     }
   }
 }
