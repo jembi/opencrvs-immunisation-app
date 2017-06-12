49c74b4e88157fd62868b7c1e706f046ec6f8f41
Martin Brocker <martin@jembi.org>
2017-06-12 13:32:46 +0200

    RCBS-57: Function to construct simple HIV confirmation object

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -1,9 +1,22 @@
 'use strict'
 
 module.exports = function () {
+  const HIV_CONFIRMATION = 'hiv-confirmation'
+
   return {
     test: () => {
       console.log('Event service test')
+    },
+
+    constructSimpleHIVConfirmationObject: (encounter, observation) => {
+      return {
+        eventType: HIV_CONFIRMATION,
+        data: {
+          partnerStatus: observation.extension[0].valueString,
+          firstPositiveHivTestLocation: encounter.location[0].location.display,
+          firstPositiveHivTestDate: observation.effectiveDateTime
+        }
+      }
     }
   }
 }
