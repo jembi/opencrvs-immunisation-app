3af73f12b039ed534686ecd7eff274e30929c491
Ryan Crichton <ryan@jembi.org>
2017-06-13 15:46:11 +0200

    Add timeline view

--- app/scripts/controllers/view-cbs-events.js
+++ app/scripts/controllers/view-cbs-events.js
@@ -1,11 +1,13 @@
 'use strict'
 
-module.exports = function ($scope, $routeParams, events, $location, Api) {
-  console.log('View CBS event controller loaded')
+const moment = require('moment')
 
-  events.test()
+module.exports = function ($scope, $routeParams, events, $location, Api) {
+  const GET_PATIENT_ERROR = 'Error: Failed to find patient with id:' + $routeParams.patientId
 
   $scope.state = {
+    patients: null,
+    singlePatient: true,
     header: {
       title: 'View patient\'s CBS timeline',
       left: [
@@ -27,18 +29,39 @@ module.exports = function ($scope, $routeParams, events, $location, Api) {
     }
   }
 
-  // API test
-  var patientId = $routeParams.patientId
+  const success = function (results) {
+    if (!results) {
+      $scope.getPatientError = GET_PATIENT_ERROR
+    }
+    $scope.state.patients = [{ resource: results }]
+  }
 
-  Api.Encounters.get({patient: patientId}, function (result) {
-    console.log(result)
-  }, function (err) {
+  const error = function (err) {
+    $scope.getPatientError = err.statusText || GET_PATIENT_ERROR
     console.error(err)
-  })
+  }
 
-  Api.Observations.get({'subject.reference': { $eq: 'Patient/' + patientId }}, function (result) {
-    console.log(result)
-  }, function (err) {
-    console.error(err)
-  })
+  Api.Patients.get({ id: $routeParams.patientId }, success, error)
+
+  $scope.events = [
+    {
+      eventTitle: 'CD4 count',
+      eventDate: moment(new Date()).fromNow()
+    },
+    {
+      eventTitle: 'Viral Load',
+      eventDate: moment(new Date('2017-06-10')).fromNow()
+    },
+    {
+      eventTitle: 'Linkage to care',
+      eventDate: moment(new Date('2017-06-03')).fromNow()
+    },
+    {
+      eventTitle: 'HIV Confirmation',
+      eventDate: moment(new Date('2017-06-01')).fromNow()
+    }
+  ]
+
+  // TODO
+  // $scope.event = events.getAllEvents($routeParams.patientId) // or similar
 }
