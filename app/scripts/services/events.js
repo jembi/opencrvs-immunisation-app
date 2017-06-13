55b290c73c97f54380fb6d81f6b43b93d42ad6fa
Andrew <napierandr@gmail.com>
2017-06-13 16:04:16 +0200

    RCBS-58: Correct structure of cd4 event

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -6,20 +6,26 @@ module.exports = function () {
     test: () => {
       console.log('Event service test')
     },
-    
+
     constructSimpleCD4CountObject: (encounter) => {
+      let providerName
+
+      encounter._observations[0].contained.forEach((containedResource) => {
+        if (containedResource.id === encounter._observations[0].performer[0].reference.substring(1)) {
+          const providerGivenName = containedResource.name[0].given.join(' ')
+          const providerFamilyName = containedResource.name[0].family.join(' ')
+          providerName = providerGivenName + ' ' + providerFamilyName
+        }
+      })
+
       return {
         eventType: CD4_COUNT,
-        encounter: {
-          date: encounter.period.start,
-          type: encounter.type[0].coding[0].display,
-          location: encounter.location[0].location.display,
-        },
-        observations: {
-          cd4CountResult: encounter._observations[0].
-        },
-        practitioner: {
-          practitioner: 'TODO'
+        eventDate: encounter.period.start,
+        data: {
+          cd4CountDate: encounter._observations[0].effectiveDateTime,
+          cd4CountLocation: encounter.location[0].location.display,
+          cd4CountResult: encounter._observations[0].valueQuantity,
+          cd4CountProvider: providerName
         }
       }
     }
--- test/services/events.js
+++ test/services/events.js
@@ -4,6 +4,7 @@ const tap = require('tap')
 
 const Events = require('../../app/scripts/services/events')
 const encounterTemplate = require('../../app/scripts/services/FHIR/resources/Encounter')
+const observationTemplate = require('../../app/scripts/services/FHIR/resources/Observation')
 
 tap.test('events .constructSimpleLinkageToCareObject should construct simple linkage to care object', (t) => {
   const events = Events()
@@ -12,11 +13,28 @@ tap.test('events .constructSimpleLinkageToCareObject should construct simple lin
   encounter.period.start = '2017-04-04'
   encounter.type[0].coding[0].display = 'ANC Visit'
   encounter.location[0].location.display = 'Chuk'
-  const event = events.constructSimpleLinkageToCareObject(encounter)
 
-  t.equals(event.eventType, 'linkage-to-care')
-  t.equals(event.encounterDate, '2017-04-04')
-  t.equals(event.encounterType, 'ANC Visit')
-  t.equals(event.encounterLocation, 'Chuk')
+  const observation = JSON.parse(JSON.stringify(observationTemplate))
+  observation.effectiveDateTime = '2017-05-05'
+  observation.valueQuantity = 'Test Result'
+  observation.performer[0].reference = '#practioner-1'
+  observation.contained = [{
+    resourceType: 'Practitioner',
+    id: 'practioner-1',
+    name: [{
+      family: ['Provider'],
+      given: ['Test']
+    }]
+  }]
+
+  encounter._observations = [observation]
+  const event = events.constructSimpleCD4CountObject(encounter)
+
+  t.equals(event.eventType, 'cd4-count')
+  t.equals(event.eventDate, '2017-04-04')
+  t.equals(event.data.cd4CountDate, '2017-05-05')
+  t.equals(event.data.cd4CountLocation, 'Chuk')
+  t.equals(event.data.cd4CountResult, 'Test Result')
+  t.equals(event.data.cd4CountProvider, 'Test Provider')
   t.end()
 })
