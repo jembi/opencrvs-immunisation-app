eadc26a87b93095ee2e15dfbc3b0ba1a79fbf28b
Martin Brocker <martin@jembi.org>
2017-06-14 13:22:23 +0200

    RCBS-78: Hookup functions to format events

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -28,6 +28,101 @@ module.exports = function (Api, $q) {
       })
   }
 
+  const constructSimpleHIVConfirmationObject = (encounter, observations) => {
+    let firstPositiveHivTestDate, partnerStatus
+
+    observations.forEach((obs) => {
+      switch (obs.code.coding[0].code) {
+        case '33660-2': // HIV test
+          firstPositiveHivTestDate = obs.effectiveDateTime
+          break
+        case 'partner-hiv-status':
+          partnerStatus = obs.valueCodeableConcept.text
+          break
+      }
+    })
+
+    return {
+      eventType: HIV_CONFIRMATION,
+      eventDate: encounter.period.start,
+      data: {
+        partnerStatus: partnerStatus,
+        firstPositiveHivTestLocation: encounter.location[0].location.display,
+        firstPositiveHivTestDate: firstPositiveHivTestDate
+      }
+    }
+  }
+
+  const constructSimpleFirstViralLoadObject = (encounter, observations) => {
+    let providerName
+
+    observations[0].contained.forEach((containedResource) => {
+      if (containedResource.id === observations[0].performer[0].reference.substring(1)) {
+        const providerGivenName = containedResource.name[0].given.join(' ')
+        const providerFamilyName = containedResource.name[0].family.join(' ')
+        providerName = providerGivenName + ' ' + providerFamilyName
+      }
+    })
+
+    return {
+      eventType: FIRST_VIRAL_LOAD,
+      eventDate: encounter.period.start,
+      data: {
+        firstViralLoadDate: observations[0].effectiveDateTime,
+        firstViralLoadResults: observations[0].valueQuantity,
+        firstViralLoadLocation: encounter.location[0].location.display,
+        firstViralLoadProvider: providerName
+      }
+    }
+  }
+
+  const constructSimpleLinkageToCareObject = (encounter) => {
+    let eventType, encounterType
+
+    encounter.type.forEach((type) => {
+      switch (type.coding[0].system) {
+        case 'http://hearth.org/event-types':
+          eventType = type.coding[0].display
+          break
+        case 'http://hearth.org/encounter-types':
+          encounterType = type.coding[0].display
+          break
+      }
+    })
+
+    return {
+      eventType: eventType,
+      eventDate: encounter.period.start,
+      data: {
+        encounterType: encounterType,
+        encounterLocation: encounter.location[0].location.display
+      }
+    }
+  }
+
+  const constructSimpleCD4CountObject = (encounter) => {
+    let providerName
+
+    encounter._observations[0].contained.forEach((containedResource) => {
+      if (containedResource.id === encounter._observations[0].performer[0].reference.substring(1)) {
+        const providerGivenName = containedResource.name[0].given.join(' ')
+        const providerFamilyName = containedResource.name[0].family.join(' ')
+        providerName = providerGivenName + ' ' + providerFamilyName
+      }
+    })
+
+    return {
+      eventType: CD4_COUNT,
+      eventDate: encounter.period.start,
+      data: {
+        cd4CountDate: encounter._observations[0].effectiveDateTime,
+        cd4CountLocation: encounter.location[0].location.display,
+        cd4CountResult: encounter._observations[0].valueQuantity,
+        cd4CountProvider: providerName
+      }
+    }
+  }
+
   return {
     sortEventsDesc: (events) => {
       return events.sort((a, b) => {
@@ -61,120 +156,37 @@ module.exports = function (Api, $q) {
         console.error(err)
         defer.reject(err)
       })
+
+      return defer.promise
+    },
+
+    formatEvents: (events) => {
+      const simpleEvents = []
+      events.forEach((event) => {
+        if (isEventOfType('linkage-to-care', event.resource)) {
+          event = constructSimpleLinkageToCareObject(event.resource, event._observations)
+        } else if (isEventOfType('hiv-confirmation', event.resource)) {
+          constructSimpleHIVConfirmationObject(event.resource, event._observations)
+        } else if (isEventOfType('cd4-count', event.resource)) {
+          constructSimpleCD4CountObject(event.resource, event._observations)
+        } else if (isEventOfType('viral-load', event.resource)) {
+          constructSimpleFirstViralLoadObject(event.resource, event._observations)
+        } else {
+          console.error('Unknown event type found', event)
+        }
+        simpleEvents.push(event)
+      })
+      return simpleEvents
     },
 
     isEventOfType: isEventOfType,
 
-    formatEvents: (events) => {
-      events.forEach((event) => {
-        if (isEventOfType('linkage-to-care', event)) {
-          // call format function
-        } else if (isEventOfType('hiv-confirmation', event)) {
-          // call format function
-        } else if (isEventOfType('cd4-count', event)) {
-          // call format function
-        } else if (isEventOfType('viral-load', event)) {
-          // call format function
-        } else {
-          console.error('Unknown event type found', event)
-        }
-      })
-      return events
-    },
+    constructSimpleHIVConfirmationObject: constructSimpleHIVConfirmationObject,
 
-    constructSimpleHIVConfirmationObject: (encounter, observations) => {
-      let firstPositiveHivTestDate, partnerStatus
+    constructSimpleFirstViralLoadObject: constructSimpleFirstViralLoadObject,
 
-      observations.forEach((obs) => {
-        switch (obs.code.coding[0].code) {
-          case '33660-2': // HIV test
-            firstPositiveHivTestDate = obs.effectiveDateTime
-            break
-          case 'partner-hiv-status':
-            partnerStatus = obs.valueCodeableConcept.text
-            break
-        }
-      })
+    constructSimpleLinkageToCareObject: constructSimpleLinkageToCareObject,
 
-      return {
-        eventType: HIV_CONFIRMATION,
-        eventDate: encounter.period.start,
-        data: {
-          partnerStatus: partnerStatus,
-          firstPositiveHivTestLocation: encounter.location[0].location.display,
-          firstPositiveHivTestDate: firstPositiveHivTestDate
-        }
-      }
-    },
-
-    constructSimpleFirstViralLoadObject: (encounter, observations) => {
-      let providerName
-
-      observations[0].contained.forEach((containedResource) => {
-        if (containedResource.id === observations[0].performer[0].reference.substring(1)) {
-          const providerGivenName = containedResource.name[0].given.join(' ')
-          const providerFamilyName = containedResource.name[0].family.join(' ')
-          providerName = providerGivenName + ' ' + providerFamilyName
-        }
-      })
-
-      return {
-        eventType: FIRST_VIRAL_LOAD,
-        eventDate: encounter.period.start,
-        data: {
-          firstViralLoadDate: observations[0].effectiveDateTime,
-          firstViralLoadResults: observations[0].valueQuantity,
-          firstViralLoadLocation: encounter.location[0].location.display,
-          firstViralLoadProvider: providerName
-        }
-      }
-    },
-
-    constructSimpleLinkageToCareObject: (encounter) => {
-      let eventType, encounterType
-
-      encounter.type.forEach((type) => {
-        switch (type.coding[0].system) {
-          case 'http://hearth.org/event-types':
-            eventType = type.coding[0].display
-            break
-          case 'http://hearth.org/encounter-types':
-            encounterType = type.coding[0].display
-            break
-        }
-      })
-
-      return {
-        eventType: eventType,
-        eventDate: encounter.period.start,
-        data: {
-          encounterType: encounterType,
-          encounterLocation: encounter.location[0].location.display
-        }
-      }
-    },
-
-    constructSimpleCD4CountObject: (encounter) => {
-      let providerName
-
-      encounter._observations[0].contained.forEach((containedResource) => {
-        if (containedResource.id === encounter._observations[0].performer[0].reference.substring(1)) {
-          const providerGivenName = containedResource.name[0].given.join(' ')
-          const providerFamilyName = containedResource.name[0].family.join(' ')
-          providerName = providerGivenName + ' ' + providerFamilyName
-        }
-      })
-
-      return {
-        eventType: CD4_COUNT,
-        eventDate: encounter.period.start,
-        data: {
-          cd4CountDate: encounter._observations[0].effectiveDateTime,
-          cd4CountLocation: encounter.location[0].location.display,
-          cd4CountResult: encounter._observations[0].valueQuantity,
-          cd4CountProvider: providerName
-        }
-      }
-    }
+    constructSimpleCD4CountObject: constructSimpleCD4CountObject
   }
 }
