7c45173a10c51963da75577b47ffd94d6c209050
Ryan Crichton <ryan@jembi.org>
2017-08-16 10:35:42 +0200

    Add patient reference to immunisation

--- app/scripts/directives/add-events/add-event/index.js
+++ app/scripts/directives/add-events/add-event/index.js
@@ -42,6 +42,7 @@ module.exports = function (loadResource, $q, state, FHIR, FormBuilderService) {
                       break
                     case 'immunisation':
                       setProcedureEventType(encounterTemplate, scope.event.code, scope.event.display)
+                      immunisationTemplate.patient.reference = scope.patient.resourceType + '/' + scope.patient.id
                       resourceTemplateDict = {
                         main: encounterTemplate,
                         location: locationTemplate,
